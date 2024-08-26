c3a4c1e09fb5be5b3767f28b8ba9abcd6f94fcb1
zymzym <zzeeemmz@gmail.com>
2024-08-27 01:10:32 +0800

    add requirement.txt  and a small function

--- data_process.py
+++ data_process.py
@@ -61,3 +61,24 @@ def get_valid_column_count(data=pd.DataFrame(), threshold: float = 0.5):
         null_ratio = data.isnull().mean()  # Calculate null ratio for each column
         valid_columns = null_ratio[null_ratio < threshold].index  # Select columns with null ratio below threshold
         return len(valid_columns)
+
+def get_valid_column(data=pd.DataFrame(), threshold: float = 0.5):
+
+    """
+    This function processes the input DataFrame based on a given threshold and returns a DataFrame result.
+
+    Parameters:
+        data (pandas.DataFrame): The input DataFrame to be processed.
+        threshold (float): A float value used as a threshold for processing the DataFrame.
+
+    Returns:
+        pandas.DataFrame: A DataFrame result based on the processing of the DataFrame with respect to the threshold.
+    """
+
+    if data.empty:
+        raise ValueError("Data is empty")
+    else:
+        null_ratio = data.isnull().mean()  # Calculate null ratio for each column
+        valid_columns = null_ratio[null_ratio < threshold].index  # Select columns with null ratio below threshold
+        data = data[[valid_columns]]
+        return data
